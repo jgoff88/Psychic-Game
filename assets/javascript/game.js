d6715b24bf90afaaa3e1e10cfc5cf1a92fd12dc0
James Goff <james_goff88@outlook.com>
2019-10-13 02:02:20 -0500

    update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,26 @@
 var letters= ['a','b','c','d','e',]
 var counter= 9;
-    if (counter != 0) {
-        var compGuess = letters[Math.floor(Math.random() * letters.length)];
-            console.log("Computers key:", compGuess);
-            document.onkeyup = function(event){
-            var userGuess= event.key.toLocaleLowerCase();
-            console.log("keypressed:", userGuess);
-            if(compGuess === userGuess){
-                alert("Good Guess");
-            } else {
-                counter -= 1; 
-                console.log("Guesses left", counter);
-                alert("Try again!");
+var wins = 0;
+var losses = 0;
+    Loop1:
+    var compGuess = letters[Math.floor(Math.random() * letters.length)];
+        console.log("Computers key:", compGuess);
+        document.onkeyup = function(event){
+        var userGuess= event.key.toLocaleLowerCase();
+        console.log("keypressed:", userGuess);
+        if(compGuess === userGuess){
+            wins ++;
+            alert("Good Guess");
+            console.log("wins:", wins);
+            continue Loop1;
+        } else if (counter == 0) {
+            alert("Your out of guesses! The answer was: " + compGuess);
+        } else {
+            counter -= 1; 
+            console.log("Guesses left", counter);
+            alert("Try again!");
 
-            }
         }
-    } else if (counter == 0) {
-        alert("Your out of guesses! The answer was " + compGuess);
     }
+
+
