c3c9774e4194dae7eb7be43943001220f967e855
James Goff <james_goff88@outlook.com>
2019-10-12 23:09:54 -0500

    update java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,20 @@
-var letters= ['a','b','c','d']
-        var compGuess = letters[0];
+var letters= ['a','b','c','d','e',]
+var counter= 9;
+    if (counter != 0) {
+        var compGuess = letters[Math.floor(Math.random() * letters.length)];
+            console.log("Computers key:", compGuess);
         document.onkeyup = function(event){
             var userGuess= event.key.toLocaleLowerCase();
             console.log("keypressed:", userGuess);
             if(compGuess === userGuess){
-                alert("You win!");
+                alert("Good Guess");
             } else {
-                alert("You Lost!");
+                counter -= 1; 
+                console.log("Guesses left", counter);
+                alert("Try again!");
+
             }
         }
+    } else {
+        alert("You Lost! Try again");
+    }
